2e7714a56e8547a5681f123c7442063457f80f86
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-03-26 21:01:00 +0530

    Starting with the password generator app completing logic for generating password with different settings

---
+++ passwordGenerator02/App.tsx
@@ -0,0 +1,73 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+
+//Form validation
+import * as Yup from 'yup';
+
+const PasswordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
+    .min(4, 'Should be minimum 4 characters')
+    .max(16, 'Should be maximum 16 characters')
+    .required('Length is required'),
+});
+
+export default function App() {
+  const [password, setPassword] = useState('');
+  const [isPasswordGenerated, setIsPasswordGenerated] = useState(false);
+  const [lowerCase, setLowerCase] = useState(true);
+  const [upperCase, setUpperCase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbols, setSymbols] = useState(false);
+
+  const generatePasswordString = (passwordLength: number) => {
+    let characterList = '';
+
+    const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const digitChars = '0123456789';
+    const specialChars = '!@#$%^&*(),./_+-=';
+
+    if (upperCase) {
+      characterList += upperCaseChars;
+    }
+    if (lowerCase) {
+      characterList += lowerCaseChars;
+    }
+    if (numbers) {
+      characterList += digitChars;
+    }
+    if (symbols) {
+      characterList += specialChars;
+    }
+
+    const passwordResult = createPassword(characterList, passwordLength);
+    setPassword(passwordResult);
+    setIsPasswordGenerated(true);
+  };
+
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
+  };
+
+  const resetPasswordState = () => {
+    setPassword('');
+    setIsPasswordGenerated(false);
+    setLowerCase(true);
+    setUpperCase(false);
+    setSymbols(false);
+    setNumbers(false);
+  };
+
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
